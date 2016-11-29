8e823a69ae8d2395814a9708a498e8e9ccfacb1e
Pehr Boman <unkelpehr@gmail.com>
2016-11-29 12:26:31 +0100

    misc

--- test.js
+++ test.js
@@ -6,6 +6,4 @@ fork3.on('child-status-change', function (child, oldStatus, newStatus) {
 
 fork3.fork('./child').send('hello', function (err) {
 	console.log('response! error:\n', err);
-}).on('error', function (err) {
-	console.loog('errroorr!', err);
 });
